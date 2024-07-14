ffb5dea44a644235778af5e1c66269d68697b292
Tiago Muller <tiagomullerdeoliveira@hotmail.com>
2024-07-14 14:46:08 -0300

    Adding description to new svc principal script

--- scripts/newServicePrincipal.sh
+++ scripts/newServicePrincipal.sh
@@ -1,1 +1,4 @@
-az ad sp create-for-rbac --name "svc-terraform-svc-principal" --role="Contributor" --scopes="/subscriptions/XXX"
+# Running this will create a new service principal to be used by terraform to create resources in your subscription
+# Be sure to save the values for clientId and clientSecret this command will output and save them to the terraform.tfvars file
+# in each folder you would like to run the template
+az ad sp create-for-rbac --name "YOUR_SVC_PRINCIPAL_NAME_HERE" --role="Contributor" --scopes="/subscriptions/YOUR_SUBSCRIPTION_NAME_HERE"
